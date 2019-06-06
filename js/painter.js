4974c75fadf19e0a8f9a5c7cad3258702d85cbfa
Javier Chames <haimch@gmail.com>
2019-06-06 14:49:57 +0300

    Main painter with colors selection works

--- js/painter.js
+++ js/painter.js
@@ -1,35 +1,84 @@
 var mainDiv = $("#paintDiv");
-var color1 = $("#PaletterBlue");
-var color2 = $("#PaletterGreen");
-var color3 = $("#PaletterYellow");
-var color4 = $("#PaletterRed");
+// var color1 = $("#PaletterBlue");
+// var color2 = $("#PaletterGreen");
+// var color3 = $("#PaletterYellow");
+// var color4 = $("#PaletterRed");
 var x;
 var y;
+var spanDot;
+var color ="black";
 
-document.getElementById("myBtn").addEventListener("click", paintColor);
+  
+var PaletterBlue=document.getElementById("PaletterBlue");
+PaletterBlue.addEventListener("click", function(){
+  color="blue"
+});
+// paintColor("blue"));
+
+var PaletterGreen =document.getElementById("PaletterGreen");
+PaletterGreen.addEventListener("click", function(){
+  color="green"
+});
+
+// PaletterGreen.addEventListener("click", paintColor("green"));
+
+var PaletterYellow =document.getElementById("PaletterYellow");
+PaletterYellow.addEventListener("click", function(){
+  color="Yellow"
+});
+
+// PaletterYellow.addEventListener("click", paintColor("yellow"));
+
+var PaletterRed =document.getElementById("PaletterRed");
+
+// PaletterRed.addEventListener("click", paintColor("red"));
+
+PaletterRed.addEventListener("click", function(){
+  color="red"
+});
+
+
+// function paintColor(selectedColor){
+  // console.log(selectedColor);
+//  if(selectedColor === "blue"){
+//    color="blue";
+//   // spanDot.css({"background-Color": "blue"});
+//  }
+//  if(selectedColor === "green"){
+//   color="green";
+//  }
+//   //  spanDot.setAttribute("background-color","blue");
+//   // css("background-color": "blue");
+
+//   console.log("blue");
+// }
+
+
 
 mainDiv.mousemove(function (e) {
   if (e.buttons == 1) {
-    showCoords();
-    draw();
+    showCoords(e);
+    draw(color);
   }
 });
 
 
-function showCoords() {
-  x = event.clientX;
-  y = event.clientY;
+function showCoords(e) {
+  x = e.clientX;
+  y = e.clientY;
+  // console.log(x,y);
 }
 
-function draw() {
-  var spanDot = $("<span/>");
+function draw(color) {
+   spanDot = $("<span/>");
   spanDot.css({
     "position": "absolute",
     "left": x,
     "top": y,
-    "width": 2 + "px",
-    "height": 2 + "px",
-    "background-color": "black"
+    "width": 3 + "px",
+    "height": 3 + "px",
+    "background-color": color
   })
   mainDiv.append(spanDot)
 }
+
