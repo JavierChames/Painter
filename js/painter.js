6c06b4b52c535be2eb4018f47c456ebd7b790f1e
Javier Chames <haimch@gmail.com>
2019-06-08 12:53:40 +0300

    Removed non needed lines in painter.js file

--- js/painter.js
+++ js/painter.js
@@ -1,48 +1,31 @@
 var mainDiv = $(".board");
-// var mainDiv = document.getElementsByClassName("board");
-
 var x;
 var y;
 var color = "black";
 
-
 var PaletterBlue = document.getElementById("PaletterBlue");
 PaletterBlue.addEventListener("click", function () {
   color = "blue"
-  // this.style.border="5px solid black";
-
-
 });
 
 var PaletterGreen = document.getElementById("PaletterGreen");
 PaletterGreen.addEventListener("click", function () {
   color = "green"
-  // this.style.border="5px solid black";
-
 });
-
 
 var PaletterYellow = document.getElementById("PaletterYellow");
 PaletterYellow.addEventListener("click", function () {
   color = "Yellow"
-  // this.style.border="5px solid black";
-
 });
-
 
 var PaletterRed = document.getElementById("PaletterRed");
 PaletterRed.addEventListener("click", function () {
   color = "red"
-  // this.style.border="5px solid black";
-
 });
 
 var PaletterBrown = document.getElementById("PaletterBrown");
-
 PaletterBrown.addEventListener("click", function () {
   color = "brown"
-  // this.style.border="5px solid black";
-
 });
 
 
@@ -50,39 +33,27 @@ function erase() {
   color = "white"
 };
 
-
-
-// mainDiv.addEventListener("mousemove", showCoords());
-// {
-
-mainDiv.on('mousemove',function(e) {
+mainDiv.on('mousemove', function (e) {
   if (e.buttons == 1) {
     showCoords(e);
     draw(color);
   }
 });
-// }
 
 function showCoords(e) {
-  if (e.buttons == 1){
-    // var posi=mainDiv.position();
-    // x1 =posi.left;
-    // y1=posi.top;
-  x = e.clientX;
-  y = e.clientY;
-  draw(color);
-  // var x1 = $(".board").position();
-    // alert("Top position: " + x1.top + " Left position: " + x1.left);
-
-  // console.log(x +","+y);
-}}
+  if (e.buttons == 1) {
+    x = e.clientX;
+    y = e.clientY;
+    draw(color);
+  }
+}
 
 function draw(color) {
   var spanDot = $("<span/>");
   spanDot.css({
     "position": "absolute",
-    "left": x- mainDiv[0].offsetLeft,
-    "top": y-mainDiv[0].offsetTop,
+    "left": x - mainDiv[0].offsetLeft,
+    "top": y - mainDiv[0].offsetTop,
     "width": slider.value + "px",
     "height": slider.value + "px",
     "background-color": color
@@ -99,8 +70,6 @@ function clearBoard() {
 var slider = document.getElementById("myRange");
 var output = document.getElementById("font");
 output.innerHTML = slider.value;
-
-slider.oninput = function() {
+slider.oninput = function () {
   output.innerHTML = this.value;
 }
-
