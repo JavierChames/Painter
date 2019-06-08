d1d6712e583679d0d608cf484be197682051a1da
Javier Chames <haimch@gmail.com>
2019-06-09 00:56:24 +0300

    Added id to each span togle class to  erase button rurn off remove when selecting new color

--- js/painter.js
+++ js/painter.js
@@ -2,36 +2,63 @@ var mainDiv = $(".board");
 var x;
 var y;
 var color = "black";
+var spanid = 1;
+// var clear = false;
 
 var PaletterBlue = document.getElementById("PaletterBlue");
 PaletterBlue.addEventListener("click", function () {
+  $("#eraser").toggleClass("selected");
   color = "blue"
 });
 
 var PaletterGreen = document.getElementById("PaletterGreen");
 PaletterGreen.addEventListener("click", function () {
+  $("#eraser").toggleClass("selected");
   color = "green"
 });
 
 var PaletterYellow = document.getElementById("PaletterYellow");
 PaletterYellow.addEventListener("click", function () {
+  $("#eraser").toggleClass("selected");
   color = "Yellow"
 });
 
 var PaletterRed = document.getElementById("PaletterRed");
 PaletterRed.addEventListener("click", function () {
+  $("#eraser").toggleClass("selected");
   color = "red"
 });
 
 var PaletterBrown = document.getElementById("PaletterBrown");
 PaletterBrown.addEventListener("click", function () {
+  $("#eraser").toggleClass("selected");
   color = "brown"
 });
 
+// var spanRemove = document.getElementById("PaletterGreen");
+// PaletterGreen.addEventListener("click", function () {
+//   $("#eraser").toggleClass("selected");
+//   color = "green"
+// });
+
+
 
 function erase() {
+  // if (clear === false) {
+  // clear = true;
+  // } else {
+  // if (clear === true) {
+  // clear = false;
+  // var idClicked = jQuery(this).attr('id');
+  // Removes an element from the document
+  // var element = document.getElementById(idClicked);
+  // element.parentNode.removeChild(element);
+  // }
+
   color = "white"
-};
+  // }
+  // }
+}
 
 mainDiv.on('mousemove', function (e) {
   if (e.buttons == 1) {
@@ -40,25 +67,53 @@ mainDiv.on('mousemove', function (e) {
   }
 });
 
+mainDiv.on('click', function (e) {
+  if (clear === true) {
+    // if (e.buttons == 1) {
+    var idClicked = jQuery(this).attr('id');
+    // Removes an element from the document
+    var element = document.getElementById(idClicked);
+    console.log(element.tagName);
+    // element.parentNode.removeChild(element);
+  }
+
+
+  // showCoords(e);
+  // draw(color);
+  // }
+
+  // var idClicked = jQuery(this).attr('id');
+  // Removes an element from the document
+  // var element = document.getElementById(idClicked);
+  // element.parentNode.removeChild(element);
+  // }
+
+
+
+
+});
+
 function showCoords(e) {
   if (e.buttons == 1) {
-    x = e.clientX;
-    y = e.clientY;
+    x = e.clientX - mainDiv[0].offsetLeft;
+    y = e.clientY - mainDiv[0].offsetTop;
     draw(color);
   }
 }
 
 function draw(color) {
+
   var spanDot = $("<span/>");
+  spanDot.attr("id", "spanid" + spanid++);
   spanDot.css({
     "position": "absolute",
-    "left": x - mainDiv[0].offsetLeft,
-    "top": y - mainDiv[0].offsetTop,
+    "left": x,
+    "top": y,
     "width": slider.value + "px",
     "height": slider.value + "px",
     "background-color": color
   })
-  mainDiv.append(spanDot)
+  mainDiv.append(spanDot);
 }
 
 function clearBoard() {
@@ -73,3 +128,7 @@ output.innerHTML = slider.value;
 slider.oninput = function () {
   output.innerHTML = this.value;
 }
+
+$("#eraser").click(function () {
+  $(this).toggleClass("selected");
+});
